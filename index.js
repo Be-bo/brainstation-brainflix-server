5d1625d5db288e7cc6bf10eb61604b0041fa6d80
Be-bo <robfiker@gmail.com>
2023-10-24 16:04:39 -0700

    Image storage server-side no working, using Multer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import multer from 'multer';
 import bodyParser from 'body-parser';
 const app = express();
 
@@ -11,11 +12,30 @@ app.use(cors({
   credentials: true, // Enable credentials (cookies, authorization headers)
 }));
 
-
 app.get("/", (req, res)=>{
 	res.send("Hello World!");
 });
 
-app.post("/upload", (req, res) => {
+
+
+// MARK: MULTER
+const storage = multer.diskStorage({ // Define storage engine and destination
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.mimetype.split('/')[1]);
+  },
+});
+const upload = multer({ storage: storage }); // upload
+app.post("/upload", upload.single('image'), (req, res) => { // upload
 	res.send("UPLOAD RECEIVED");
+});
+
+
+
+// MARK: START LISTENING
+app.listen(80, ()=>{
+	console.log("Server running on port 80.");
 });
