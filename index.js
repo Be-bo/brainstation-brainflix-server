71df39cd46299bff58dfd3a4f9f8701046d50674
Be-bo <robfiker@gmail.com>
2023-10-25 17:11:33 -0700

    Adjusted API design to fit assignment parameters. Filled all REST functions, filled out almost all supporting functions.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-// MARK: VARS
 import express from 'express';
 import cors from 'cors';
 import multer from 'multer';
 import bodyParser from 'body-parser';
 import fs from 'fs';
+import { error } from 'console';
+import { rejects } from 'assert';
 const app = express();
+
+
+// MARK: VARS
+const videosPath = '/uploads/videos.json';
+const videoDetailsPath = '/uploads/video-details.json';
+const imageBucketPath = 'uploads';
 
 
 // MARK: SETUP
@@ -19,43 +26,59 @@ app.use(
 
 
 // MARK: FUNCTIONS
-function updateDatabase(newData) {
-  const filePath = 'db.json';
+function getVideoPreviewInfo(jsonPayload){
+  // TODO: gotta extract from the json payload
+}
 
-  return new Promise((resolve, reject) => {
+function uploadNewVideoData(jsonPayload) {
+  return new Promise(async (resolve, reject) => {
+    try{
 
-    fs.readFile(filePath, (error, data) => {
-      if (error) {
-        console.log(error);
-        reject(error);
-      } else {
-        const json = JSON.parse(data);
+      let localJSON = await fs.promises.readFile(videoDetailsPath);
+      let parsedJSON = JSON.parse(localJSON);
+      parsedJSON.items.push(jsonPayload); // assuming the uploaded json payload will already be in the video-details format
+      await fs.promises.writeFile(videoDetailsPath, JSON.stringify(parsedJSON, null, 2));
 
-        json.items.push(newData);
+      localJSON = await fs.promises.readFile(videosPath);
+      parsedJSON = JSON.parse(localJSON);
+      parsedJSON.items.push(getVideoPreviewInfo(jsonPayload));
+      await fs.promises.writeFile(videosPath, JSON.stringify(parsedJSON, null, 2));
 
-        fs.writeFile(filePath, JSON.stringify(json, null, 2), (writeError) => {
-          if (writeError) {
-            console.log(writeError);
-            reject(writeError);
-          } else resolve();
-        });
-      }
-    });
+      resolve();
+
+    }catch (error){
+      console.log(error);
+      reject(error);
+    }
   });
 }
 
-function appendVideos(){
-
+function appendToComments(newComment, videoId){
+  return new Promise(async (resolve, reject) =>{
+    try{
+      const fileData = await fs.promises.readFile(videoDetailsPath);
+      const parsedJSON = JSON.parse(fileData);
+      const videoObject = parsedJSON.items.find((item) => {
+        return item.id == videoId;
+      });
+      videoObject.comments.push(newComment);
+      await fs.promises.writeFile(videoDetailsPath, JSON.stringify(parsedJSON, null, 2));
+      resolve();
+    }catch(error){
+      console.log(error);
+      reject(error);
+    }
+  });
 }
 
-function appendVideoDetails(){
-
+function getStringifiedVideosJSON(){
 }
 
-function appendComments(){
-
+function getStringifiedVideoDetailsJSON(videoId){
 }
 
+function getThumbnailLocalPath(thumbnailURL){
+}
 
 
 // MARK: SAVING FILES
@@ -78,49 +101,74 @@ const upload = multer({ storage: storage });
 // MARK: HELLO WORLD
 app.get('/', (req, res) => {
   console.log('Hello World!');
-  res.send('Hello World!');
+  res.send('Hello World!'); // non JSON / plan tuxt
 });
 
 
 // MARK: GET VIDEOS INFO
-app.get('/videos', (req, res) => {});
+app.get('/videos', async (req, res) => {
+  try{
+    const returnJSON = await getStringifiedVideosJSON();
+    res.json({
+      message: 'Video info list retrieved successfully.',
+      data: returnJSON,
+    })
+  }catch(error){
+    res.status(500).json({error: 'Failed to retrieve video info list.'})
+  }
+});
 
 
 // MARK: GET VIDEO THUMBNAIL
-app.get('videos/:videoId/thumbnail', (req, res) => {
+app.get('videos/:videoId/thumbnail', async (req, res) => {
   const videoId = req.params.videoId;
+  try{
+    const imagePath = await getThumbnailLocalPath(videoId);
+    res.setHeader('Content-Type', 'image/*');
+    res.sendFile(imagePath);
+  }catch(error){
+    res.status(500).json({error: 'Failed to retrieve thumbnail image.'});
+  }
 });
 
 
 // MARK: GET VIDEO DETAILS
-app.get('/videos/:videoId', (req, res) => {
+app.get('/videos/:videoId', async (req, res) => {
   const videoId = req.params.videoId;
+  try{
+    const returnJSON = await getStringifiedVideoDetailsJSON(videoId);
+    res.json({
+      message: 'Video data retrieved succesfully.',
+      data: returnJSON,
+    });
+  }catch(error){
+    res.status(500).json({ error: 'Failed to retrieve video details.' });
+  }
 });
 
 
 // MARK: POST COMMENT
-app.post('/videos/:videoId/comment', (req, res) => {
+app.post('/videos/:videoId/comment', async (req, res) => {
   const videoId = req.params.videoId;
+  try{
+    const parsedComment = JSON.parse(req.body.json);
+    await appendToComments(parsedComment, videoId);
+    res.json({ message: 'Comment added successfully.'});
+  }catch(error){
+    res.status(500).json({error: 'Failed to add comment'});
+  }
 });
 
 
 // MARK: POST VIDEO
-app.post('/upload', upload.single('image'), (req, res) => {
-  console.log('Uploaded JSON data:', req.body.json);
-
-  const jsonData = JSON.parse(req.body.json);
-
-  updateDatabase(jsonData)
-    .then(() => {
-      res.json({
-        message: 'File uploaded and JSON data received successfully.',
-      });
-    })
-    .catch((error) => {
-      res.status(500).json({
-        error: 'Failed to update JSON database',
-      });
-    });
+app.post('/upload', upload.single('image'), async (req, res) => {
+  try{
+    const parsedJSON = JSON.parse(req.body.json);
+    await uploadNewVideoData(parsedJSON);
+    res.json({message: 'File uploaded and JSON data received successfully.',});
+  }catch(error){
+    res.status(500).json({error: 'Failed to update JSON database',});
+  }
 });
 
 
